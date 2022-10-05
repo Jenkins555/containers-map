d14c7a2285cb494f47bede418a5b5f991930c04d
Анисимов Александр <ujc90@mail.ru>
2022-10-05 10:47:33 +0300

    edit classError.js

--- src/js/classError.js
+++ src/js/classError.js
@@ -4,12 +4,9 @@ export class ErrorRepository {
     this.errors = new Map([[1, 'error001'], [2, 'error002'], [3, 'error003'], [4, 'error004'], [5, 'error005']]);
   }
 
-  translate(code) { /// Интерация внутри map
-    /* eslint-disable-next-line */
-    for (const [key, value] of this.errors) {
-      if (key === code) {
-        return value;
-      }
+  translate(code) {
+    if (this.errors.has(code)) {
+      return this.errors.get(code);
     }
     return 'Unknown error';
   }
